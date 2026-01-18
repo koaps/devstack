a0f394258044213e0dcde8d449dd656aafeac8b7
Koaps <koaps@users.noreply.github.com>
2026-01-18 10:55:49 -0800

    Sync main
    Files:
      - docker-compose.yml
      - rebuild_service.sh

--- rebuild_service.sh
+++ rebuild_service.sh
@@ -6,4 +6,4 @@ if [ -z $service ]; then echo "E: need to pass service name"; exit 1; fi
 docker-compose -p devstack rm -s $service
 docker-compose -p devstack pull $service
 docker-compose -p devstack build $service
-docker-compose -p devstack up -d $service
+docker-compose -p devstack up --no-deps -d $service
