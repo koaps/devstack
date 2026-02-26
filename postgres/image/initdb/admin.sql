9fed05e62d41b4971edd73be70f9160d078f4d82
Koaps <koaps@users.noreply.github.com>
2026-02-26 10:27:17 -0800

    Sync test
    Files:
      - postgres/image/initdb/admin.sql
      - vault/docker-compose.yml

--- postgres/image/initdb/admin.sql
+++ postgres/image/initdb/admin.sql
@@ -2,3 +2,4 @@ create user admin with encrypted password 'adm1n';
 grant usage, create on schema public to admin;
 grant all privileges on database drone to admin;
 grant all privileges on database gitea to admin;
+grant all privileges on database vault to admin;
