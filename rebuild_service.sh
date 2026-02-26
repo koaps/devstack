1d5c0a5486e1b90d5b708660fe7fc9e3dc3b7265
Koaps <koaps@users.noreply.github.com>
2026-02-26 12:27:38 -0800

    Sync test
    Files:
      - postgres/docker-compose.yml
      - rebuild_service.sh
      - vault/docker-compose.yml
      - vault/vault.conf

--- rebuild_service.sh
+++ rebuild_service.sh
@@ -5,7 +5,7 @@ project=devstack
 service="$1"
 if [ -z $service ]; then echo "E: need to pass service name"; exit 1; fi
 
-$compose -p $project rm -s $service
+$compose -p $project rm -sf $service
 $compose -p $project pull $service
 $compose -p $project build $service
 $compose -p $project up --no-deps -d $service
